df3f79e0e591b8e781ddec7df1e67268c4023b4a
Kostya Ten <kostya@yandex.ru>
2022-01-20 20:06:56 +0300

    pref: first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="img/logo_vertera.svg" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
